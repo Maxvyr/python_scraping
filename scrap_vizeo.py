de20e99753b6ce833a70bb2f34016c23d1b430ca
Maxvyr <maxvid30@gmail.com>
2022-01-31 19:53:59 +0100

    :construction: scrap title of each product

--- scrap_vizeo.py
+++ scrap_vizeo.py
@@ -3,22 +3,37 @@ import requests
 from bs4 import BeautifulSoup
 
 
-URL  = "https://vizeo.eu/Cameras.html"
-page = requests.get(URL)
-resultProductPage = []
+URL_BASE  = "https://vizeo.eu/"
+URL_CAM  = "Cameras.html"
+page = requests.get(URL_BASE + URL_CAM)
+result_product_per_page = []
 
-if page.status_code == 200:
+
+def recoverAllProduc():
+    if page.status_code == 200:
+            soup = BeautifulSoup(page.content, "html.parser")
+            result = soup.find_all('a', href=True)
+            hrefs = []
+            for link in result:
+                hrefs.append(link['href'])
+            # print(hrefs)
+            sub_string = "FicheProduit-"
+            for ref_page_prod in hrefs:
+                if sub_string in ref_page_prod:
+                    result_product_per_page.append(ref_page_prod)
+    else:
+        print(f"Page not found : {page.status_code}")
+
+def changePage():
+    for page_cam in result_product_per_page:
+        page = requests.get(URL_BASE + page_cam)
+        # print(page.status_code)
         soup = BeautifulSoup(page.content, "html.parser")
-        result = soup.find_all('a', href=True)
-        hrefs = []
-        for link in result:
-            hrefs.append(link['href'])
-        # print(hrefs)
-        subString = "FicheProduit-"
-        for refProd in hrefs:
-            if subString in refProd:
-                # print(refProd)
-                resultProductPage.append(refProd)
-        print(resultProductPage)
-else:
-    print(f"Page not found : {page.status_code}")
+        result = soup.title
+        print(result)   
+
+recoverAllProduc()
+print("next")
+changePage()
+print("end")
+
